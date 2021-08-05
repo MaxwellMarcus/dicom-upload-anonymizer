09e4fcfa4b7b5f2aa9f476a59e93628422793157
Dakota Castleberg <dakota@radiologics.com>
2021-08-05 13:39:52 -0500

    added total volume size counter and warning

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,31 +1,31 @@
 import { useState } from 'react'
-import { script } from '../../utils'
+import { script, formatFileSize } from '../../utils'
 import JSZip from 'jszip'
 import Anonymizer from 'dicomedit'
 import Dropzone from 'react-dropzone'
 import './Upload.module.css'
-import Grid from '@material-ui/core/Grid';
-
+import Grid from '@material-ui/core/Grid'
 
 function Upload() {
   const [files, setFiles] = useState([])
   const [progress, setProgress] = useState(0)
   const [totalFiles, setTotalFiles] = useState(0)
 
+  let progressCounter = 0
+  let totalCounter = 0
+  let totalVolume = 0
+
   const onFileUpload = async (uploaded) => {
-
-
     // If a zipped folder is uploaded
     if (uploaded[0].type.includes('zip')) {
       const zip = new JSZip()
-      let totalCounter = 0
-      let progressCounter = 0
 
       zip.loadAsync(uploaded[0]).then((zip) => {
         zip.forEach((relativePath, file) => {
           if (relativePath.includes('dcm')) {
             totalCounter++
             const fileName = file.name
+            const size = file._data.uncompressedSize
             zip
               .file(file.name)
               .async('arraybuffer')
@@ -36,7 +36,7 @@ function Upload() {
                 const output = anonymizer.outputDict
                 progressCounter++
                 setProgress(progressCounter)
-                setFiles((files) => [...files, { fileName, output }])
+                setFiles((files) => [...files, { fileName, size, output }])
                 console.log(fileName, anonymizer.outputDict)
                 // arrayBuffer = anonymizer.write()
               })
@@ -46,21 +46,20 @@ function Upload() {
       })
     } else {
       // one/many not-zipped dcm files are uploaded
-      let progressCounter = 0
       for (let i = 0; i < uploaded.length; i++) {
         setTotalFiles(uploaded.length)
         let reader = new FileReader()
         const anonymizer = new Anonymizer(script)
         let file = uploaded[i]
         let fileName = file.name
-
+        let size = file.size
         reader.onload = async function () {
           anonymizer.loadDcm(reader.result)
           await anonymizer.applyRules()
           const output = anonymizer.outputDict
           progressCounter++
           setProgress(progressCounter)
-          setFiles((files) => [...files, { fileName, output }])
+          setFiles((files) => [...files, { fileName, size, output }])
           console.log(file.name, anonymizer.outputDict)
           // const response = await UploadService(file)
           // console.log('response ', response)
@@ -80,6 +79,10 @@ function Upload() {
     }
   }
 
+  if (files.length > 0) {
+    files.forEach((file) => (totalVolume += file.size))
+  }
+
   return (
     <>
       <div>
@@ -89,12 +92,15 @@ function Upload() {
 
       <Grid container justifyContent='center'>
         <Grid item xs={6}>
-          <Dropzone onDrop={acceptedFiles => onFileUpload(acceptedFiles)}>
+          <Dropzone onDrop={(acceptedFiles) => onFileUpload(acceptedFiles)}>
             {({ getRootProps, getInputProps }) => (
               <section>
                 <div {...getRootProps()}>
                   <input {...getInputProps()} />
-                  <p>Drag&apos;n&apos;drop some files here, or click to select files</p>
+                  <p>
+                    Drag&apos;n&apos;drop some files here, or click to select
+                    files
+                  </p>
                 </div>
               </section>
             )}
@@ -104,6 +110,11 @@ function Upload() {
 
       <br />
 
+      <p style={{ color: 'red' }}>
+        If the (unzipped) total folder/file size exceeds 2 GB, expect the page
+        to crash
+      </p>
+      <p>total upload size - {formatFileSize(totalVolume)}</p>
       <p>{totalFiles} files selected</p>
       <p>{progress} uploaded and anonymized</p>
 
